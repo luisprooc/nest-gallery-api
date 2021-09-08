29cf6e91b4a96c535d927a73d30b885da23cce8c
Luis Rosario <luisprooc@gmail.com>
2021-09-08 10:16:56 -0400

    [ADD] Swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('v1/api')
+  const config = new DocumentBuilder()
+  .setTitle('Gallery API')
+  .setDescription('The Gallery API description')
+  .setVersion('1.0')
+  .addTag('documentation')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(AppModule.port || 3000);
 }
 bootstrap();
