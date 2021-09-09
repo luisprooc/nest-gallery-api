4b7d7e4f39e5fb5086166991ce1efe9714595219
Luis Rosario <luisprooc@gmail.com>
2021-09-09 10:15:53 -0400

    [ADD] User controller added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { User } from 'src/modules/database/entities/user.entity';
 import { UserService } from 'src/modules/user/user.service';
 
 @Controller('users')
@@ -9,4 +10,28 @@ export class UserController {
   findAll(): Promise<any[]> {
     return this._userService.getAll();
   }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this._userService.findOne(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this._userService.delete(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number, 
+    @Body() updateUser: User
+  ) {
+    return this._userService.update(id,updateUser);
+  }
+
+  @Post()
+  create(@Body() createUser: User) {
+    return this._userService.create(createUser);
+  }
+
 }
